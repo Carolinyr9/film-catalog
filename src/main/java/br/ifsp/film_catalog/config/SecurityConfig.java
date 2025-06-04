478bfc279f5fcada47bf1c69e367cf67208e9956
Bowie <carolinyr9@gmail.com>
2025-06-04 15:35:27 -0300

    ♻️ refactor SecurityConfig to properly use @EnableWebSecurity

--- src/main/java/br/ifsp/film_catalog/config/SecurityConfig.java
+++ src/main/java/br/ifsp/film_catalog/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +24,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig {
     
     @Value("${jwt.public.key}")
